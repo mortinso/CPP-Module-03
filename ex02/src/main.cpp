2b8cdac0031f3351772d8cf2ad452ac8126589d0
mortinso <89762267+mortinso@users.noreply.github.com>
2024-05-31 18:25:54 +0100

    ex02: Updated main.cpp

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,40 +6,45 @@
 /*   By: mortins- <mortins-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 15:39:17 by mortins-          #+#    #+#             */
-/*   Updated: 2024/05/31 17:19:51 by mortins-         ###   ########.fr       */
+/*   Updated: 2024/05/31 18:16:50 by mortins-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "../inc/ClapTrap.hpp"
 #include "../inc/ScavTrap.hpp"
+#include "../inc/FragTrap.hpp"
 
 int	main(void) {
-	ClapTrap ray("Ray");
+	ClapTrap paul("Paul");
 	ScavTrap jake("Jake");
+	FragTrap rosa("Rosa");
 
 	std::cout << std::endl;
 
+	paul.showStats();
 	jake.showStats();
-	ray.beRepaired(15);
-	ray.showStats();
+	rosa.showStats();
 	
 	std::cout << std::endl;
 
-	jake.attack(ray.getName());
-	ray.takeDamage(jake.getDamage());
-	ray.attack(jake.getName());
-	jake.takeDamage(ray.getDamage());
-	jake.beRepaired(5);
+	rosa.attack(jake.getName());
+	jake.takeDamage(rosa.getDamage());
+	paul.beRepaired(5);
+	jake.attack(paul.getName());
+	paul.takeDamage(jake.getDamage());
+	paul.beRepaired(5);
 	
 	std::cout << std::endl;
 	
-	ray.showStats();
+	paul.showStats();
 	jake.showStats();
+	rosa.showStats();
 	
 	std::cout << std::endl;
 	
 	jake.guardGate();
+	rosa.highFivesGuys();
 	
 	std::cout << std::endl;
 }
